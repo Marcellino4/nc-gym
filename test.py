7977d013e9ef3ec520e7eecee2c8ef542583b5f1
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2024-06-14 13:03:56 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import serial
+
+serial_port = '/dev/rfcomm0'  # Pastikan path ini sesuai dengan perangkat yang terhubung
+
+try:
+    ser = serial.Serial(serial_port)  # Membuka koneksi serial
+    ser.write(b'1')  # Menulis data ke port serial
+    ser.close()  # Menutup koneksi serial
+    print("Data telah dikirim.")
+except serial.SerialException as e:
+    print(f"Terjadi kesalahan saat membuka atau menggunakan port serial: {e}")
+except Exception as e:
+    print(f"Terjadi kesalahan: {e}")
