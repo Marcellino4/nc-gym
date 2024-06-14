e2166e8205be5c4b745926f35c0faf184df5edd7
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2024-06-14 13:05:14 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import serial
 
-serial_port = '/dev/rfcomm0'  # Pastikan path ini sesuai dengan perangkat yang terhubung
+serial_port = '/dev/rfcomm1'  # Pastikan path ini sesuai dengan perangkat yang terhubung
 
 try:
     ser = serial.Serial(serial_port)  # Membuka koneksi serial
