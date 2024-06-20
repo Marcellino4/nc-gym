4a62b63e269d2cf8a5519c043e3a5896db3360c2
Arxent <danielchristian81@gmail.com>
2024-06-20 12:09:08 +0700

    Revert "fix: logging bt5.py"
    This reverts commit 7649b7ab50d371fe1624803f4aeaf9a5e9767300.

--- bt5.py
+++ bt5.py
@@ -3,11 +3,10 @@ import requests
 import serial
 import threading
 import datetime
+import logging
 
-# Fungsi untuk menulis log ke file
-def write_log(message):
-    with open('log_gate.log', 'a') as f:
-        f.write(f"{datetime.datetime.now()} - {message}\n")
+# Konfigurasi logging
+logging.basicConfig(filename='logfilepy.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 # Inisialisasi pemetaan key secara dinamis
 key_mappings = {}
@@ -21,13 +20,13 @@ for name in dir(ecodes):
 # Fungsi untuk menangani input dan logika untuk setiap keyboard
 def handle_keyboard(device_path, status, serial_port):
     keyboard = InputDevice(device_path)
-    write_log(f"Listening on {device_path} (device name: {keyboard.name})")
+    logging.info(f"Listening on {device_path} (device name: {keyboard.name})")
 
     try:
         while True:
             current_time = datetime.datetime.now()
             id_gabung = ''  # Inisialisasi ulang variabel untuk menyimpan kata
-            write_log(f"\nReady for new input on {device_path}. Press ENTER to submit.")
+            logging.info(f"\nReady for new input on {device_path}. Press ENTER to submit.")
 
             for event in keyboard.read_loop():
                 if event.type == ecodes.EV_KEY:
@@ -39,7 +38,7 @@ def handle_keyboard(device_path, status, serial_port):
                             if karakter == "ENTER":
                                 break
                             else:
-                                write_log(karakter)  # Print the modified key name for demo purposes
+                                logging.info(karakter)  # Print the modified key name for demo purposes
                                 id_gabung += karakter  # Concatenate the modified key name
 
             if id_gabung.strip():
@@ -55,24 +54,24 @@ def handle_keyboard(device_path, status, serial_port):
                                 ser = serial.Serial(serial_port)
                                 ser.write(b'1')
                                 ser.close()
-                                write_log(f"Serial data sent for {device_path} at {current_time}.")
+                                logging.info(f"Serial data sent for {device_path} at {current_time}.")
                             except serial.SerialException as e:
-                                write_log(f"Serial port error: {e}")
+                                logging.error(f"Serial port error: {e}")
                         else:
-                            write_log(f"API response was not true for {device_path} at {current_time}. Waiting for next input...\n")
+                            logging.info(f"API response was not true for {device_path} at {current_time}. Waiting for next input...\n")
                     except requests.exceptions.RequestException as e:
-                        write_log(f"HTTP Request error: {e}")
+                        logging.error(f"HTTP Request error: {e}")
                 else:
                     # Membuka port serial dan mengirimkan data
                     try:
                         ser = serial.Serial(serial_port)
                         ser.write(b'1')
                         ser.close()
-                        write_log(f"Serial data sent for {device_path} at {current_time}.")
+                        logging.info(f"Serial data sent for {device_path} at {current_time}.")
                     except serial.SerialException as e:
-                        write_log(f"Serial port error: {e}")
+                        logging.error(f"Serial port error: {e}")
     except KeyboardInterrupt:
-        write_log(f"\nExiting program for {device_path}.")
+        logging.info(f"\nExiting program for {device_path}.")
 
 # Membuat dan menjalankan threads untuk kedua keyboard
 # masuk
