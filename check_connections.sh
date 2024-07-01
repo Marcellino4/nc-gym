7ed6b0782924927fcd0d9e4f1744582c12a80215
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2024-07-01 17:14:38 +0700

    Update check_connections.sh

--- check_connections.sh
+++ check_connections.sh
@@ -2,7 +2,7 @@
 
 # Fungsi untuk mengecek status koneksi perangkat Bluetooth
 check_connections() {
-  local addresses=("DC:0D:30:93:BF:8C" "DC:0D:30:93:BF:11" "98:D3:31:FB:5E:5C" "98:D3:31:FB:5F:57")
+  local addresses=("DC:0D:30:93:BF:11" "DC:0D:30:93:BF:8C" "98:D3:31:FB:5F:57" "98:D3:31:FB:5E:5C")
   local connected_addresses=($(hcitool con | grep -oP '([0-9A-F]{2}:){5}[0-9A-F]{2}'))
   
   for address in "${addresses[@]}"; do
