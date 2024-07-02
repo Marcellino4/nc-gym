6617a49a1585bd35e0904a96a0c5be8038210858
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2024-07-02 18:06:10 +0700

    Update command.py

--- command.py
+++ command.py
@@ -16,9 +16,28 @@ logger = logging.getLogger(__name__)
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hello! I am your bot. Use /restart to restart the Bluetooth service.')
 
+# Fungsi untuk menangani perintah /restart
+# def restart(update: Update, context: CallbackContext) -> None:
+#     chat_id = update.message.chat_id
+#     user = update.message.from_user
+#     logger.info(f"User {user.first_name} issued /restart command")
+
+#     # Mengirim pesan konfirmasi ke Telegram
+#     update.message.reply_text('Restarting Bluetooth service...')
+
+#     # Menjalankan perintah sistem
+#     try:
+#         subprocess.run(['sudo', 'systemctl', 'restart', 'bluetooth'], check=True)
+#         subprocess.run(['sudo', 'systemctl', 'daemon-reload'], check=True)
+#         subprocess.run(['sudo', 'systemctl', 'restart', 'connect-bluetooth.service'], check=True)
+#         update.message.reply_text('Bluetooth service restarted successfully.')
+#     except subprocess.CalledProcessError as e:
+#         logger.error(f"Error restarting services: {e}")
+#         update.message.reply_text(f'Failed to restart Bluetooth service: {e}')
+
 def main() -> None:
     # Membuat updater dan pass the bot's token.
-    updater = Updater(TELEGRAM_BOT_TOKEN)
+    updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
 
     # Mendapatkan dispatcher untuk mendaftarkan handler
     dispatcher = updater.dispatcher
