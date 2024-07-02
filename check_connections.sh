b0720ee5578a5f17f1660d40080012fe8ed78bfd
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2024-07-02 17:33:39 +0700

    Update check_connections.sh

--- check_connections.sh
+++ check_connections.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Token API Telegram Anda
+TELEGRAM_BOT_TOKEN="7243366231:AAGxqP4QhS_cPv1-JHfN5NbFrT1wk7Y-TBk"
+# Ganti dengan chat ID yang Anda dapatkan dari getUpdates
+CHAT_ID="-1002204066531"
+
+# Fungsi untuk mengirim pesan ke Telegram
+send_telegram_message() {
+  local message=$1
+  curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+       -d chat_id=${CHAT_ID} \
+       -d text="${message}" \
+       -d parse_mode="HTML"
+}
+
 # Fungsi untuk mengecek status koneksi perangkat Bluetooth
 check_connections() {
   local addresses=("DC:0D:30:93:BF:11" "DC:0D:30:93:BF:8C" "98:D3:31:FB:5F:57" "98:D3:31:FB:5E:5C")
@@ -8,12 +22,17 @@ check_connections() {
   for address in "${addresses[@]}"; do
     if [[ ! " ${connected_addresses[@]} " =~ " ${address} " ]]; then
       echo "$(date): Device $address tidak terhubung. Restarting Bluetooth service..." &>> /var/www/nc-gym/logfile.log
+      send_telegram_message "Device $address tidak terhubung. Restarting Bluetooth service..."
       sudo systemctl restart bluetooth
       sudo systemctl daemon-reload
       sudo systemctl restart connect-bluetooth.service
+      echo "Menunggu 15 detik sebelum memeriksa koneksi lagi..." &>> /var/www/nc-gym/logfile.log
+      sleep 15
+      check_connections
       return 1
     else
       echo "$(date): Device $address terhubung." &>> /var/www/nc-gym/logfile.log
+      send_telegram_message "Device $address terhubung."
     fi
   done
   return 0
