d863229ebe4308d0b783e36752a624fdd022d1e5
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2024-07-02 21:48:04 +0700

    Update check_connections.sh

--- check_connections.sh
+++ check_connections.sh
@@ -43,12 +43,11 @@ check_connections() {
   local addresses=("DC:0D:30:93:BF:11" "DC:0D:30:93:BF:8C" "98:D3:31:FB:5F:57" "98:D3:31:FB:5E:5C")
   local connected_addresses=($(hcitool con | grep -oP '([0-9A-F]{2}:){5}[0-9A-F]{2}'))
   local hcitool_output=$(hcitool con)
-  
   local loopnum = 0;
 
   for address in "${addresses[@]}"; do
     send_telegram_message "connectnya : ${address}"
-    send_telegram_message "harusnya : ${connected_addresses[0]}"
+    send_telegram_message "harusnya : ${connected_addresses[loopnum]}"
     if [[ ! " ${connected_addresses[@]} " =~ " ${address} " ]]; then
       echo "$(date): Device $address tidak terhubung. Restarting Bluetooth service..." &>> /var/www/nc-gym/logfile.log
 
@@ -68,7 +67,7 @@ check_connections() {
       echo "$(date): Device $address terhubung." &>> /var/www/nc-gym/logfile.log
       # send_telegram_message2 "Device $address terhubung."
     fi
-    loopnum = 
+    loopnum=$((loopnum + 1))
   done
   return 0
 }
