ba6aa9b4c707768373afebc88494bb24e4b5d223
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2024-07-02 17:53:15 +0700

    Update check_connections.sh

--- check_connections.sh
+++ check_connections.sh
@@ -29,6 +29,15 @@ send_telegram_message() {
        -d parse_mode="MarkdownV2" >/dev/null 2>&1
 }
 
+
+send_telegram_message2() {
+  local message=$1
+  curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+       -d chat_id=${CHAT_ID} \
+       -d text="${message}" \
+       -d parse_mode="HTML"
+}
+
 # Fungsi untuk mengecek status koneksi perangkat Bluetooth
 check_connections() {
   local addresses=("DC:0D:30:93:BF:11" "DC:0D:30:93:BF:8C" "98:D3:31:FB:5F:57" "98:D3:31:FB:5E:5C")
@@ -36,14 +45,14 @@ check_connections() {
   local hcitool_output=$(hcitool con)
 
   # Mengirim hasil dari hcitool con ke Telegram
-  send_telegram_message "Hasil dari hcitool con:\n\`\`\`${hcitool_output}\`\`\`"
+  send_telegram_message "Hasil dari hcitool con:\`\`\`${hcitool_output}\`\`\`"
 
   for address in "${addresses[@]}"; do
     if [[ ! " ${connected_addresses[@]} " =~ " ${address} " ]]; then
       echo "$(date): Device $address tidak terhubung. Restarting Bluetooth service..." &>> /var/www/nc-gym/logfile.log
 
       # Mengirim hasil dari hcitool con ke Telegram
-      send_telegram_message "Device $address tidak terhubung. Restarting Bluetooth service..."
+      send_telegram_message2 "Device $address tidak terhubung. Restarting Bluetooth service..."
 
       sudo systemctl restart bluetooth
       sudo systemctl daemon-reload
@@ -54,7 +63,7 @@ check_connections() {
       return 1
     else
       echo "$(date): Device $address terhubung." &>> /var/www/nc-gym/logfile.log
-      send_telegram_message "Device $address terhubung."
+      send_telegram_message2 "Device $address terhubung."
     fi
   done
   return 0
