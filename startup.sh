85c074b5532cb3eb3e05d482ac35c627c10a931e
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2024-09-04 14:55:59 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -14,32 +14,4 @@ send_telegram_message() {
     curl -s -X POST $API_URL -d chat_id=$TELEGRAM_CHAT_ID -d text="$message"
 }
 
-echo "Starting Bluetooth connection..."
-send_telegram_message "Starting Bluetooth connection..."
-bluetoothctl connect DC:0D:30:93:BF:11
-
-# Tunggu beberapa detik agar bluetoothctl selesai
-sleep 10
-
-echo "Connecting rfcomm0 (Masuk)..."
-send_telegram_message "Connecting rfcomm0..."
-sudo rfcomm connect rfcomm0 98:D3:31:FB:5E:5C
-
-sleep 10
-
-echo "Connecting rfcomm1 (Keluar)..."
-send_telegram_message "Connecting rfcomm1..."
-sudo rfcomm connect rfcomm1 98:D3:31:FB:5F:57
-
-# Tunggu beberapa detik agar rfcomm selesai
-sleep 10
-
-echo "Running Python scripts..."
-send_telegram_message "Running Python scripts Masuk..."
-cd /var/www/nc-gym && python test.py
-send_telegram_message "Running Python scripts Keluar..."
-cd /var/www/nc-gym && python test2.py
-sleep 10
-
-# Kirimkan log ke Telegram setelah semua perintah selesai
-send_telegram_message "Startup script completed. Log:\n$(cat $LOGFILE)"
+send_telegram_message "Starting Orangepi..."
