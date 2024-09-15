26945935d1041994329f3142a24a88006df2d56f
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2024-09-15 17:44:30 +0700

    Update testingcloud.py

--- testingcloud.py
+++ testingcloud.py
@@ -1,13 +1,14 @@
-import requests
-import time
+import cloudscraper
 
+# Inisialisasi scraper
+scraper = cloudscraper.create_scraper()
+
+# URL yang ingin diakses
 url = "https://nc-gym.com"
-response = requests.get(url)
 
-if "Please wait while your request is being verified" in response.text:
-    print("Waiting for Cloudflare verification...")
-    time.sleep(10)  # Tunggu 10 detik lalu coba lagi
-    response = requests.get(url)
+# Melakukan request menggunakan cloudscraper
+response = scraper.get(url)
 
+# Tampilkan hasil status dan response dari API
 print(f"Status Code: {response.status_code}")
 print(f"Response API: {response.text}")
