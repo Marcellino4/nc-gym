b3c32e4d00c1febfa298a4e53a268e384853ec03
marcelloktavian <110073625+Marcellino4@users.noreply.github.com>
2025-05-10 10:41:50 +0700

    feat: new config

---
+++ deepseek_bash_20250510_f39240.sh
@@ -0,0 +1,5 @@
+sudo systemctl daemon-reload
+sudo systemctl enable rfcomm0.service
+sudo systemctl enable rfcomm1.service
+sudo systemctl enable masuk.service
+sudo systemctl enable keluar.service
---
+++ deepseek_bash_20250510_ff7844.sh
@@ -0,0 +1,4 @@
+sudo systemctl start rfcomm0.service
+sudo systemctl start rfcomm1.service
+sudo systemctl start masuk.service
+sudo systemctl start keluar.service
